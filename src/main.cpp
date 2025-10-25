012dca261b66182733a32c90f01d30f8795a6f3b
ApersonJ <vitolajesus54@gmail.com>
2025-10-25 14:56:33 -0500

    Cambios basicos
    Agrego lo basiquito

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "../include/funciones.h"
+
+using namespace std;
+
+main(){
+
+    return 0;
+}
